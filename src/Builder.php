38fb348984b2bffed246dea6aee3c92d56cdfce5
Mike Bronner <mike@genealabs.com>
2017-09-21 07:53:37 -0700

    Refactor builder method to cache relationships

--- src/Builder.php
+++ src/Builder.php
@@ -3,6 +3,9 @@
 use Closure;
 use Illuminate\Cache\TaggableStore;
 use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Eloquent\Relations\Pivot;
+use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
 class Builder extends EloquentBuilder
 {
@@ -12,24 +15,27 @@ class Builder extends EloquentBuilder
         $relation->addEagerConstraints($models);
         $constraints($relation);
 
+        return $relation->match(
+            $relation->initRelation($models, $name),
+            $this->cacheResults($relation, $models),
+            $name
+        );
+    }
+
+    protected function cacheResults(Relation $relation, array $models) : Collection
+    {
         $parentIds = implode('_', collect($models)->pluck('id')->toArray());
         $parentName = str_slug(get_class($relation->getParent()));
         $childName = str_slug(get_class($relation->getRelated()));
-        // $cache = cache();
-        //
-        // if (is_subclass_of($cache->getStore(), TaggableStore::class)) {
-        //     $cache->tags([$parentName, $childName]);
-        // }
+        $cache = cache();
 
-        $results = cache()->tags([$parentName, $childName])
+        if (is_subclass_of($cache->getStore(), TaggableStore::class)) {
+            $cache->tags([$parentName, $childName]);
+        }
+
+        return cache()->tags([$parentName, $childName])
             ->rememberForever("{$parentName}_{$parentIds}-{$childName}s", function () use ($relation) {
                 return $relation->getEager();
             });
-
-        return $relation->match(
-            $relation->initRelation($models, $name),
-            $results,
-            $name
-        );
     }
 }
