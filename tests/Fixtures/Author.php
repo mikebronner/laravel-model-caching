049b5cfa9ab3c7f433c71a1986245664d09f1ccd
Mike Bronner <mike@genealabs.com>
2017-11-19 09:24:22 -0800

    Add test of query scopes in cache key

--- tests/Fixtures/Author.php
+++ tests/Fixtures/Author.php
@@ -1,6 +1,7 @@
 <?php namespace GeneaLabs\LaravelModelCaching\Tests\Fixtures;
 
 use GeneaLabs\LaravelModelCaching\CachedModel;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
@@ -19,5 +20,10 @@ class Author extends CachedModel
     public function profile() : HasOne
     {
         return $this->hasOne(Profile::class);
+    }
+
+    public function scopeStartsWithA(Builder $query) : Builder
+    {
+        return $query->where('name', 'LIKE', 'A%');
     }
 }
