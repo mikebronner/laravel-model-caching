7316cceedb10ee7a2cbb219afa6b9a33e6fa12e7
Ivan Mykhavko Tegos <tegosiv@gmail.com>
2018-11-23 16:12:02 +0200

    fix notice `Undefined index: operator` #69

--- src/CacheKey.php
+++ src/CacheKey.php
@@ -97,7 +97,7 @@ class CacheKey
 
     protected function getTypeClause($where) : string
     {
-        $type = in_array($where["type"], ["In", "NotIn", "Null", "NotNull", "between", "NotInSub", "InSub", "JsonContains"])
+        $type = in_array($where["type"], ["InRaw", "In", "NotIn", "Null", "NotNull", "between", "NotInSub", "InSub", "JsonContains"])
             ? strtolower($where["type"])
             : strtolower($where["operator"]);
 
