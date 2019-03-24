22c94109706652ce8346d8a84d60eb4a3980b671
Mike Harrison <mike.harrison@custompermits.com>
2019-03-24 17:20:44 -0400

    feat: add tests for cooldown disable options

---
+++ tests/Integration/DisabledCooldownTest.php
@@ -0,0 +1,50 @@
+<?php namespace GeneaLabs\LaravelModelCaching\Tests\Integration;
+
+use GeneaLabs\LaravelModelCaching\Tests\Fixtures\Author;
+use GeneaLabs\LaravelModelCaching\Tests\IntegrationTestCase;
+use Illuminate\Cache\Events\CacheMissed;
+use Illuminate\Support\Facades\Event;
+
+/**
+* @SuppressWarnings(PHPMD.TooManyPublicMethods)
+* @SuppressWarnings(PHPMD.TooManyMethods)
+ */
+class DisabledCooldownTest extends IntegrationTestCase
+{
+
+    public function testCooldownCacheIsNotSearchedWhenSetInConfig()
+    {
+    	$config = config('laravel-model-cache');
+
+    	$config['cooldown-disable'][] = 'App\Author';
+
+    	config($config);
+
+	    Event::fake();
+
+    	Author::first();
+
+    	Event::assertNotDispatched(CacheMissed::class, function(CacheMissed $event){
+			return $event->key === 'genealabs:laravel-model-caching::App\Author-cooldown:seconds';
+	    });
+    }
+
+    public function testCooldownCacheIsNotSearchedWhenNotEnabled()
+    {
+	    $config = config('laravel-model-cache');
+
+	    $config['enable-cooldown'] = false;
+
+	    config($config);
+
+	    Event::fake();
+
+	    Author::first();
+
+	    Event::assertNotDispatched(CacheMissed::class, function(CacheMissed $event){
+		    return $event->key === 'genealabs:laravel-model-caching::App\Author-cooldown:seconds';
+	    });
+    }
+
+
+}
