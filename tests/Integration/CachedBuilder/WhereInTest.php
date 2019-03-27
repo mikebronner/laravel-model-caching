c5324ef2ce237860c24860a22ef14276c7e45ff4
Binyomin Greenes <bygreenes@gmail.com>
2019-03-27 12:56:23 -0400

    handle whereIn when collection is empty

--- tests/Integration/CachedBuilder/WhereInTest.php
+++ tests/Integration/CachedBuilder/WhereInTest.php
@@ -41,4 +41,30 @@ class WhereInTest extends IntegrationTestCase
         $this->assertEquals($liveResults->pluck("id"), $books->pluck("id"));
         $this->assertEquals($liveResults->pluck("id"), $cachedResults->pluck("id"));
     }
+
+
+    public function testWithInWhenSetIsEmpty()
+    {
+        $key = sha1('genealabs:laravel-model-caching:testing::memory::books:genealabslaravelmodelcachingtestsfixturesbook-author_id_in_1_2_3_4');
+        $tags = [
+            'genealabs:laravel-model-caching:testing::memory::genealabslaravelmodelcachingtestsfixturesbook',
+        ];
+        $authors = (new UncachedAuthor)
+            ->where("id", "<", 0)
+            ->get(["id"]);
+
+        $books = (new Book)
+            ->whereIn("author_id", $authors)
+            ->get();
+        $cachedResults = $this
+                             ->cache()
+                             ->tags($tags)
+                             ->get($key)['value'];
+        $liveResults = (new UncachedBook)
+            ->whereIn("author_id", $authors)
+            ->get();
+
+        $this->assertEquals($liveResults->pluck("id"), $books->pluck("id"));
+        $this->assertNull($cachedResults);
+    }
 }
