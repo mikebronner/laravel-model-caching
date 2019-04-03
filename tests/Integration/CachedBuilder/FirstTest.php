d1947f200174c0bffa0c1552b24ec8948f7937b9
Mike Bronner <mike@genealabs.com>
2019-04-03 13:21:42 -0700

    Add test to make sure `first()` is not the same as `all()`

--- tests/Integration/CachedBuilder/FirstTest.php
+++ tests/Integration/CachedBuilder/FirstTest.php
@@ -19,13 +19,27 @@ class FirstTest extends IntegrationTestCase
 {
     public function testFirstReturnsAllAttributesForModel()
     {
-        $author = (new Author)->where("id", "=", 1)->first();
-        $uncachedAuthor = (new UncachedAuthor)->where("id", "=", 1)->first();
+        $author = (new Author)
+            ->where("id", "=", 1)
+            ->first();
+        $uncachedAuthor = (new UncachedAuthor)
+            ->where("id", "=", 1)
+            ->first();
 
         $this->assertEquals($author->id, $uncachedAuthor->id);
         $this->assertEquals($author->created_at, $uncachedAuthor->created_at);
         $this->assertEquals($author->updated_at, $uncachedAuthor->updated_at);
         $this->assertEquals($author->email, $uncachedAuthor->email);
         $this->assertEquals($author->name, $uncachedAuthor->name);
+    }
+
+    public function testFirstIsNotTheSameAsAll()
+    {
+        $authors = (new Author)
+            ->all();
+        $author = (new Author)
+            ->first();
+
+        $this->assertNotEquals($authors, $author);
     }
 }
