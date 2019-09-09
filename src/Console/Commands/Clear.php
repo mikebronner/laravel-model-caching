9406673f4ace08c7c34701458ce95ad4caa6a16a
Mike Bronner <mike@genealabs.com>
2019-09-09 06:10:48 -0700

    Initial code to flush by database.

--- src/Console/Commands/Clear.php
+++ src/Console/Commands/Clear.php
@@ -1,23 +1,30 @@
 <?php namespace GeneaLabs\LaravelModelCaching\Console\Commands;
 
+use GeneaLabs\LaravelModelCaching\CachedModel;
 use Illuminate\Console\Command;
 use Illuminate\Container\Container;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
 
 class Clear extends Command
 {
-    protected $signature = 'modelCache:clear {--model=}';
+    protected $signature = 'modelCache:clear {--model=} {--database=}';
     protected $description = 'Flush cache for a given model. If no model is given, entire model-cache is flushed.';
 
     public function handle()
     {
-        $option = $this->option('model');
+        $model = $this->option('model');
+        $database = $this->option('database');
 
-        if (! $option) {
-            return $this->flushEntireCache();
+        if ($model) {
+            return $this->flushModelCache($model);
         }
 
-        return $this->flushModelCache($option);
+        if ($database) {
+            return $this->flushDatabaseCache($database);
+        }
+
+        return $this->flushEntireCache();
     }
 
     protected function flushEntireCache() : int
@@ -36,21 +43,49 @@ class Clear extends Command
         return 0;
     }
 
-    protected function flushModelCache(string $option) : int
+    protected function flushDatabaseCache(string $database) : int
     {
-        $model = new $option;
-        $usesCachableTrait = $this->getAllTraitsUsedByClass($option)
+        $useDatabaseKeying = Container::getInstance()
+            ->make("config")
+            ->get("laravel-model-caching.use-database-keying");
+        
+        if (! $useDatabaseKeying) {
+            $this->error("Database keying must be enabled in 'config/laravel-model-caching.php'.");
+            $this->line("No caches were flushed.");
+
+            return 1;
+        }
+
+        // get all tags
+        $tags = collect()
+            ->filter(function ($tag) use ($database) {
+                $parts = explode(":", $tag);
+
+                return ($parts[3] ?? "") === $database;
+            })
+            ->toArray();
+
+        (new CachedModel)->flushCache($tags);
+        $this->info("✔︎ Cache for database '{$database}' has been flushed.");
+
+        return 0;
+    }
+
+    protected function flushModelCache(string $modelClass) : int
+    {
+        $model = new $modelClass;
+        $usesCachableTrait = $this->getAllTraitsUsedByClass($modelClass)
             ->contains("GeneaLabs\LaravelModelCaching\Traits\Cachable");
 
         if (! $usesCachableTrait) {
-            $this->error("'{$option}' is not an instance of CachedModel.");
+            $this->error("'{$modelClass}' is not an instance of CachedModel.");
             $this->line("Only CachedModel instances can be flushed.");
 
             return 1;
         }
 
         $model->flushCache();
-        $this->info("✔︎ Cache for model '{$option}' has been flushed.");
+        $this->info("✔︎ Cache for model '{$modelClass}' has been flushed.");
 
         return 0;
     }
