f747e611173d761ef5aa6378a2d5b481d4e62c29
Mike Bronner <mike@genealabs.com>
2026-02-28 09:15:39 -0700

    Update CacheKey.php

--- src/CacheKey.php
+++ src/CacheKey.php
@@ -140,16 +140,18 @@ class CacheKey
         $type = strtolower($where["type"]);
         $subquery = $this->getValuesFromWhere($where);
 
-        if (! is_numeric($subquery) && ! is_numeric(str_replace("_", "", $subquery))) {
-            if (strlen($subquery) === 16) {
-                try {
-                    $subquery = Uuid::fromBytes($subquery);
-                    $values = $this->recursiveImplode([$subquery], "_");
+        if (
+            ! is_numeric($subquery)
+            && ! is_numeric(str_replace("_", "", $subquery))
+            && strlen($subquery) === 16
+        ) {
+            try {
+                $subquery = Uuid::fromBytes($subquery);
+                $values = $this->recursiveImplode([$subquery], "_");
 
-                    return "-{$where["column"]}_{$type}{$values}";
-                } catch (Throwable $exception) {
-                    // do nothing
-                }
+                return "-{$where["column"]}_{$type}{$values}";
+            } catch (Throwable) {
+                // do nothing
             }
         }
 
