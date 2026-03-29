dae121a27f69d1e006d0b1d1732ac58e9ebd526e
Mike Bronner <mike@genealabs.com>
2026-03-29 08:11:03 -0700

    🚧 chore: initialize branch for issue #583 (#584)

---
+++ tests/Fixtures/IntegerStatus.php
@@ -0,0 +1,7 @@
+<?php namespace GeneaLabs\LaravelModelCaching\Tests\Fixtures;
+
+enum IntegerStatus: int
+{
+    case Active = 1;
+    case Inactive = 0;
+}
---
+++ tests/Fixtures/StringStatus.php
@@ -0,0 +1,7 @@
+<?php namespace GeneaLabs\LaravelModelCaching\Tests\Fixtures;
+
+enum StringStatus: string
+{
+    case Active = 'active';
+    case Inactive = 'inactive';
+}
